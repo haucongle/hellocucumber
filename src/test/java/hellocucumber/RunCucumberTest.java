9428c8994fee15ecb72aececf7514a10328ec047
Hau Le <haule@hoiio.com>
2018-11-11 16:46:05 +0700

    Update RunCucumberTest.java

--- src/test/java/hellocucumber/RunCucumberTest.java
+++ src/test/java/hellocucumber/RunCucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        plugin = {"pretty"},
+        plugin = {"pretty", "json:target/cucumber.json"},
 //        monochrome = true,
 //        dryRun = true,
 //        strict = true,
